30a0324aaca27b158b401fb1e5b49c1100035487
rvpermyakov <permyakov.rooma@gmail.com>
2019-09-27 03:45:16 +0500

    Task Three

---
+++ Function/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fucntion
+{
+    class Program
+    {
+        static private double F(double x)
+        {
+            if (x < 0)
+            {
+                return -x;
+            }
+            else if (x < 1)
+            {
+                return x;
+            }
+            else if (x < 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return 5 - 2 * x;
+            }
+
+        }
+
+        static int Main()
+        {
+            {
+                double x;
+                x = double.Parse(Console.ReadLine());
+                Console.Write(F(x));
+                return 0;
+            }
+
+        }
+    }
+}
